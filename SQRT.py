76ec7c3f0a58d16ddf06792f4c30a83cdf270535
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-11-20 19:03:41 +0530

    first commit

---
+++ SQRT.py
@@ -0,0 +1,11 @@
+# creating the algorithm
+# displaying the title
+print("|Square Root finder|")
+# disclaimer
+print("NOTE: Do not use a decimal number.")
+num = int(input("Enter a number: "))
+# processing the num
+ans = num ** 0.5
+# displaying the answer
+print("The square root of",num,"is",ans)
+print("Thank you for using SQRT python.")
